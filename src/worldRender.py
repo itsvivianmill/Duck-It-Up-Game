80412771fc76008934180a1c834bd8c0603cf08e
QuanD <quandohoang23@gmail.com>
2023-11-04 12:40:36 -0700

    uhefs

---
+++ src/worldRender.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class world:
+    def __init__(self, rx,ry,spriteX,spriteY):
+        self.rx = rx
+        self.ry = ry
+        self.spriteRX = spriteX
+        self.spriteRY = spriteY
+
+
+        
