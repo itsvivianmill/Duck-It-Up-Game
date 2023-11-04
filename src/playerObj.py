d8476f5b5ea31440c8234c267498c6e07dccec78
crazycoderLucy <lucyhe6767@gmail.com>
2023-11-04 15:46:24 -0700

    Create playerObj.py

---
+++ src/playerObj.py
@@ -0,0 +1,26 @@
+import pygame
+class playerObj():
+    def __init__(self,spriteSet):
+      self.x = 0
+      self.y = 0
+      self.rotation = 0
+      self.moveSpeed =0.1
+      self.spriteSet
+
+
+    def playerMove(self):
+        keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.y -= self.moveSpeed
+            print('up')
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.y += self.moveSpeed
+            print('down')
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.x -= self.moveSpeed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.x += self.moveSpeed
+
+
+    
