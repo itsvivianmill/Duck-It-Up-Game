f766f116710a430e5dc83f8ef91d6f7c0c03b4ee
QuanD <quandohoang23@gmail.com>
2023-11-04 15:22:51 -0700

    tiles

--- src/main.py
+++ src/main.py
@@ -6,20 +6,22 @@ screen = pygame.display.set_mode((300, 300), flags=pygame.RESIZABLE)
 pygame.display.set_caption('duck')   
 
 tileMapAssetList = [
-    r"src\asset\mapTiles\grass.png",
-    r"src\asset\mapTiles\grassFlower.png",
-    r"src\asset\mapTiles\grassFlower2.png",
-    r"src\asset\mapTiles\grassTree.png",
-    r"src\asset\mapTiles\rock.png",
-    r"src\asset\mapTiles\water.png",
-    r"src\asset\mapTiles\boderbottom.png",
-    r"src\asset\mapTiles\borderleft.png",
-    r"src\asset\mapTiles\borderright.png",
-    r"src\asset\mapTiles\bordertop.png",
-    r"src\asset\mapTiles\cornertopright.png",
-    r"src\asset\mapTiles\cornertopleft.png",
-    r"src\asset\mapTiles\cornerbottomright.png",
-    r"src\asset\mapTiles\cornerbottomleft.png"
+    r"src\asset\mapTiles\grass.png",            #0
+    r"src\asset\mapTiles\grassFlower.png",      #1
+    r"src\asset\mapTiles\grassFlower2.png",     #2
+    r"src\asset\mapTiles\grassTree.png",        #3
+    r"src\asset\mapTiles\rock.png",             #4
+    r"src\asset\mapTiles\water.png",            #5
+    r"src\asset\mapTiles\boderbottom.png",      #6
+    r"src\asset\mapTiles\borderleft.png",       #7
+    r"src\asset\mapTiles\borderright.png",      #8
+    r"src\asset\mapTiles\bordertop.png",        #9
+    r"src\asset\mapTiles\cornertopright.png",   #10
+    r"src\asset\mapTiles\cornertopleft.png",    #11
+    r"src\asset\mapTiles\cornerbottomright.png",#12
+    r"src\asset\mapTiles\cornerbottomleft.png", #13
+    r"src\asset\mapTiles\watertipleft.png",     #14
+    r"src\asset\mapTiles\watertipright.png"     #15
 ]
 
 tilemap = tileset.tileset(tileMapAssetList)
